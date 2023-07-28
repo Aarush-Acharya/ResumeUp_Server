8ad83dfa19b6552e5e125f87c257e007dd424960
Aarush Acharya <92685647+Aarush-Acharya@users.noreply.github.com>
2023-07-28 23:36:45 +0530

    fix: Json object not serializable

--- server.py
+++ server.py
@@ -40,7 +40,7 @@ def getChart():
     payload = {}
     headers = {}
     response = requests.request("GET", url, headers=headers, data=payload)
-    return  jsonify({"Status": response.content})
+    return  jsonify({"Status": response.content.decode('utf-8')})
 
 @app.route("/deploy")
 def deploy():
